de278444e4e0988435f6e538c840ad45a06883de
瑋浩  呂 <hiroshiryo@weihaode-MacBook-Air.local>
2018-07-11 00:08:33 +0800

    add time limit

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ client.on('message', msg => {
 	if (msg.author.bot) return;
 	if (msg.channel.id !== gfchannel) return;
 	if (!searchflag) {
-		msg.reply('冷卻中');
+		msg.reply('冷卻中').then(message => {message.delete(1000)});
 		return;
 	}
 
