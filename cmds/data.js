d2915f46ad696159a3603030813da8af4a4d577b
jeff <hiroshiryo@weihaode-MacBook-Air.local>
2018-08-22 00:04:17 +0800

    fix

--- cmds/data.js
+++ cmds/data.js
@@ -2,19 +2,19 @@ const json = require('jsonfile');
 
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb+srv://admin:eddie84567@test-mjoxl.mongodb.net/test?retryWrites=true", { useNewUrlParser: true }, function (err, db) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        db.db("tset").collection("test", (err, collection) => {
-            console.log(collection);
-        });
+// MongoClient.connect("mongodb+srv://admin:eddie84567@test-mjoxl.mongodb.net/test?retryWrites=true", { useNewUrlParser: true }, function (err, db) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         db.db("tset").collection("test", (err, collection) => {
+//             console.log(collection);
+//         });
 
-        console.log('connected');
-        db.close();
-    }
-});
+//         console.log('connected');
+//         db.close();
+//     }
+// });
 
 
 
